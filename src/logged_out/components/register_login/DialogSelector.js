dca22d6aa7cf0cddb2886620a9ad6b4bf60de96b
dunky11 <tim.v.kaenel@web.de>
2020-02-06 21:25:43 +0100

    deleted some unused code

--- src/logged_out/components/register_login/DialogSelector.js
+++ src/logged_out/components/register_login/DialogSelector.js
@@ -4,32 +4,12 @@ import RegisterDialog from "./RegisterDialog";
 import TermsOfServiceDialog from "./TermsOfServiceDialog";
 import LoginDialog from "./LoginDialog";
 import ModalBackdrop from "../../../shared/ModalBackdrop";
-import SendPasswordEmailDialog from "./SendPasswordEmailDialog";
 import ChangePasswordDialog from "./ChangePasswordDialog";
-import SendNewVerificationEmailDialog from "./SendNewVerificationEmailDialog";
 
 class DialogSelector extends PureComponent {
   state = {
-    lastEmail: null,
     loginStatus: null,
-    registerStatus: null,
-    newPasswordEmail: null
-  };
-
-  componentDidMount() {
-    const { openChangePasswordDialog } = this.props;
-    const url = window.location.href;
-    if (
-      url.includes("resetpassword") &&
-      url.includes("selector") &&
-      url.includes("token")
-    ) {
-      openChangePasswordDialog();
-    }
-  }
-
-  setNewPasswordEmail = newPasswordEmail => {
-    this.setState({ newPasswordEmail });
+    registerStatus: null
   };
 
   setRegisterStatus = registerStatus => {
@@ -40,28 +20,10 @@ class DialogSelector extends PureComponent {
     this.setState({ loginStatus });
   };
 
-  setLastEmail = lastEmail => {
-    this.setState({ lastEmail });
-  };
-
   onClose = () => {
     const { onClose } = this.props;
     this.setState({ loginStatus: null, registerStatus: null });
     onClose();
-  };
-
-  openSendPasswordEmailDialog = (mode = false) => {
-    const { openSendPasswordEmailDialog } = this.props;
-    const { lastEmail } = this.state;
-    this.setState({
-      registerStatus: null,
-      loginStatus: null,
-      // We have to define a specific mode here, because if we
-      // check for true, an event is often passed by the onClick function
-      // and the event is thruthy
-      newPasswordEmail: mode === "useLastEmail" ? lastEmail : null
-    });
-    openSendPasswordEmailDialog();
   };
 
   printDialog = () => {
@@ -70,25 +32,17 @@ class DialogSelector extends PureComponent {
       openTermsDialog,
       openRegisterDialog,
       openLoginDialog,
-      setLoggedIn,
-      openSendNewVerificationEmailDialog
+      setLoggedIn
     } = this.props;
-    const {
-      lastEmail,
-      loginStatus,
-      registerStatus,
-      newPasswordEmail
-    } = this.state;
+    const { loginStatus, registerStatus } = this.state;
     switch (dialogOpen) {
       case "register":
         return (
           <RegisterDialog
             onClose={this.onClose}
             openTermsDialog={openTermsDialog}
-            setLastEmail={this.setLastEmail}
             status={registerStatus}
             setStatus={this.setRegisterStatus}
-            openSendPasswordEmailDialog={this.openSendPasswordEmailDialog}
           />
         );
       case "termsOfService":
@@ -98,25 +52,8 @@ class DialogSelector extends PureComponent {
           <LoginDialog
             onClose={this.onClose}
             setLoggedIn={setLoggedIn}
-            lastEmail={lastEmail}
-            setLastEmail={this.setLastEmail}
-            openSendPasswordEmailDialog={this.openSendPasswordEmailDialog}
             status={loginStatus}
             setStatus={this.setLoginStatus}
-            openSendNewVerificationEmailDialog={
-              openSendNewVerificationEmailDialog
-            }
-          />
-        );
-      case "sendPasswordEmail":
-        return (
-          <SendPasswordEmailDialog
-            setLoginStatus={this.setLoginStatus}
-            onClose={openLoginDialog}
-            lastEmail={lastEmail}
-            setLastEmail={this.setLastEmail}
-            setNewPasswordEmail={this.setNewPasswordEmail}
-            newPasswordEmail={newPasswordEmail}
           />
         );
       case "changePassword":
@@ -125,13 +62,6 @@ class DialogSelector extends PureComponent {
             onClose={openLoginDialog}
             setLoginStatus={this.setLoginStatus}
             openSendPasswordEmailDialog={this.openSendPasswordEmailDialog}
-          />
-        );
-      case "sendNewVerificationEmail":
-        return (
-          <SendNewVerificationEmailDialog
-            onClose={openLoginDialog}
-            setLoginStatus={this.setLoginStatus}
           />
         );
       default:
@@ -155,10 +85,7 @@ DialogSelector.propTypes = {
   setLoggedIn: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
   openTermsDialog: PropTypes.func.isRequired,
-  openRegisterDialog: PropTypes.func.isRequired,
-  openSendPasswordEmailDialog: PropTypes.func.isRequired,
-  openChangePasswordDialog: PropTypes.func.isRequired,
-  openSendNewVerificationEmailDialog: PropTypes.func.isRequired
+  openRegisterDialog: PropTypes.func.isRequired
 };
 
 export default DialogSelector;
