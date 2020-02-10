c53c38044fb9e022e27c8e5170c8e618fb286d5a
dunky11 <tim.v.kaenel@web.de>
2020-02-10 13:47:00 +0100

    Removed unused Jss

--- src/logged_in/components/subscription/Subscription.js
+++ src/logged_in/components/subscription/Subscription.js
@@ -1,36 +1,8 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import { List, Divider, Paper, withStyles } from "@material-ui/core";
+import { List, Divider, Paper } from "@material-ui/core";
 import SubscriptionTable from "./SubscriptionTable";
 import SubscriptionInfo from "./SubscriptionInfo";
-
-const styles = theme => ({
-  RoutingInnerArea: {
-    margin: theme.spacing(1),
-    width: "auto",
-    [theme.breakpoints.up("xs")]: {
-      width: "90%",
-      marginLeft: "auto",
-      marginRight: "auto",
-      marginTop: theme.spacing(4),
-      marginBottom: theme.spacing(4)
-    },
-    [theme.breakpoints.up("sm")]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      width: "70%",
-      marginLeft: "auto",
-      marginRight: "auto"
-    },
-    [theme.breakpoints.up("md")]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      width: "50%",
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
-  }
-});
 
 class Subscription extends PureComponent {
   componentDidMount() {
@@ -53,8 +25,8 @@ class Subscription extends PureComponent {
 }
 
 Subscription.propTypes = {
-  transactions: PropTypes.array,
+  transactions: PropTypes.array.isRequired,
   selectSubscription: PropTypes.func.isRequired
 };
 
-export default withStyles(styles, { withTheme: true })(Subscription);
+export default Subscription;
