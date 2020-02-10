e478314e7101547fcac51cd41cca79e1e6b3dd68
dunky11 <tim.v.kaenel@web.de>
2020-02-11 00:20:22 +0100

    Some work on the AddBalanceDialog

--- src/logged_in/components/subscription/AddBalanceDialog.js
+++ src/logged_in/components/subscription/AddBalanceDialog.js
@@ -1,61 +1,82 @@
-import React, { Fragment } from "react";
+import React, { Fragment, PureComponent } from "react";
 import PropTypes from "prop-types";
-import { TextField, Grid, Button, OutlinedInput } from "@material-ui/core";
+import { TextField, Grid, Button, InputAdornment } from "@material-ui/core";
 import FormDialog from "../../../shared/FormDialog";
 
-function AddBalanceDialog(props) {
-  const { open } = props;
-  return (
-    <FormDialog
-      open={open}
-      headline="Add Balance"
-      content={
-        <Fragment>
-          <Grid container spacing={0}>
-            <Grid item xs={8}>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                label="Your Name"
-                fullWidth
-                autoFocus
-                autoComplete="off"
-                type="email"
-                FormHelperTextProps={{ error: true }}
-              />
+class AddBalanceDialog extends PureComponent {
+  state = { value: 0 };
+
+  onChange = event => {
+    const { value } = event.target;
+    if (value >= 0) {
+      this.setState({ value: event.target.value });
+    }
+  };
+
+  render() {
+    const { open } = this.props;
+    const { value } = this.state;
+    return (
+      <FormDialog
+        open={open}
+        headline="Add Balance"
+        content={
+          <Fragment>
+            <Grid container spacing={0} justify="space-between">
+              <Grid item xs={8}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  label="Your Name"
+                  fullWidth
+                  autoFocus
+                  autoComplete="off"
+                  type="text"
+                  FormHelperTextProps={{ error: true }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <TextField
+                  required
+                  value={value}
+                  onChange={this.onChange}
+                  variant="outlined"
+                  fullWidth
+                  type="number"
+                  margin="normal"
+                  label="amount"
+                  style={{ marginTop: 16, marginBottom: 8 }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">$</InputAdornment>
+                    )
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Credit Card"
+                  autoFocus
+                  autoComplete="off"
+                  FormHelperTextProps={{ error: true }}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <OutlinedInput
-                fullWidth
-                type="number"
-                margin="normal"
-                style={{ marginTop: 16, marginBottom: 8 }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                label="Your Name"
-                autoFocus
-                autoComplete="off"
-                type="email"
-                FormHelperTextProps={{ error: true }}
-              />
-            </Grid>
-          </Grid>
-        </Fragment>
-      }
-      actions={
-        <Button fullWidth variant="contained" color="secondary" size="large">
-          Update
-        </Button>
-      }
-    />
-  );
+          </Fragment>
+        }
+        actions={
+          <Button fullWidth variant="contained" color="secondary" size="large">
+            Update
+          </Button>
+        }
+      />
+    );
+  }
 }
 
 AddBalanceDialog.propTypes = {
---
+++ src/logged_in/components/subscription/CardTextField.js
@@ -0,0 +1,7 @@
+import { PureComponent } from "react";
+
+const styles = theme => ({});
+
+class CardTextField extends PureComponent {}
+
+export default withStyle(styles, { withTheme: true })(CardTextField);
