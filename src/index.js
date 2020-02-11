29f135d7ff25f59b7d3fe2209bdfc676888e78dd
dunky11 <tim.v.kaenel@web.de>
2020-02-11 03:31:19 +0100

    Transformed Pace css into jss

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import React, { Fragment, Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Theme from "./Theme";
+import theme from "./theme";
 import GlobalStyles from "./GlobalStyles";
 import * as serviceWorker from "./serviceWorker";
-import "./shared/pace";
+import Pace from "./shared/Pace";
 
 const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
 
@@ -15,13 +15,14 @@ const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
 
 ReactDOM.render(
   <BrowserRouter>
-    <MuiThemeProvider theme={Theme}>
+    <MuiThemeProvider theme={theme}>
       <link
         href="https://fonts.googleapis.com/css?family=Baloo+Bhaijaan|Roboto:300,400,500"
         rel="stylesheet"
       />
       <CssBaseline />
       <GlobalStyles />
+      <Pace color={theme.palette.primary.light} />
       <Suspense fallback={<Fragment />}>
         <Switch>
           <Route path="/c">
