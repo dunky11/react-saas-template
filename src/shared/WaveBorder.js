5166f572598ca363e03d4be4b6f4e14544bd5ba7
dunky11 <tim.v.kaenel@web.de>
2020-02-11 03:59:08 +0100

    Moved WaveBorder css into jss

--- src/shared/WaveBorder.js
+++ src/shared/WaveBorder.js
@@ -1,13 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../css/waveAnimation.css";
+import { withStyles } from "@material-ui/core";
 
+const styles = {
+  waves: {
+    position: "relative",
+    width: "100%",
+    height: "15vh",
+    marginBottom: "-7px",
+    minHeight: 100,
+    maxHeight: 150
+  },
+  "@keyframes moveForever": {
+    from: { transform: "translate3d(-90px, 0, 0)" },
+    to: { transform: "translate3d(85px, 0, 0)" }
+  },
+  parallax: {
+    "& > use": {
+      animation: "$moveForever 4s cubic-bezier(0.62, 0.5, 0.38, 0.5) infinite",
+      animationDelay: "-2s"
+    }
+  }
+};
+
+/**
+ *  https://codepen.io/csspoints/pen/WNeOEqd
+ */
 function WaveBorder(props) {
-  const { className, lowerColor, upperColor } = props;
+  const { className, lowerColor, upperColor, classes } = props;
   return (
     <div className={className} style={{ background: upperColor }}>
       <svg
-        className="waves"
+        className={classes.waves}
         xmlns="http://www.w3.org/2000/svg"
         xlink="http://www.w3.org/1999/xlink"
         viewBox="0 24 150 28"
@@ -20,7 +44,7 @@ function WaveBorder(props) {
             d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
           />
         </defs>
-        <g className="parallax">
+        <g className={classes.parallax}>
           <use href="#gentle-wave" x="48" y="0" fill={lowerColor} />
         </g>
       </svg>
@@ -31,7 +55,8 @@ function WaveBorder(props) {
 WaveBorder.propTypes = {
   lowerColor: PropTypes.string.isRequired,
   upperColor: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
   className: PropTypes.string
 };
 
-export default WaveBorder;
+export default withStyles(styles)(WaveBorder);
