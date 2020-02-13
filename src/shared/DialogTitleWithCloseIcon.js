2859255cf58adb272163d0c9b83eafd4fcb72d87
dunky11 <tim.v.kaenel@web.de>
2020-02-13 01:14:24 +0100

    Jss -> Box

--- src/shared/DialogTitleWithCloseIcon.js
+++ src/shared/DialogTitleWithCloseIcon.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import {
   IconButton,
   DialogTitle,
@@ -21,22 +20,27 @@ function DialogTitleWithCloseIcon(props) {
   return (
     <DialogTitle
       style={{
-        paddingBottom: paddingBottom ? paddingBottom : theme.spacing(3)
+        paddingBottom: paddingBottom
+          ? paddingBottom && disablePadding
+            ? 0
+            : paddingBottom
+          : theme.spacing(3),
+        paddingLeft: disablePadding ? 0 : null,
+        paddingRight: disablePadding ? 0 : null,
+        paddingTop: disablePadding ? 0 : theme.spacing(2)
       }}
-      className={classNames(
-        "d-flex justify-content-between w-100",
-        disablePadding ? "p-0" : "pt-2"
-      )}
       disableTypography
     >
-      <Typography variant="h5">{title}</Typography>
-      <IconButton
-        onClick={onClose}
-        style={{ marginRight: -12, marginTop: -10 }}
-        disabled={disabled}
-      >
-        <CloseIcon />
-      </IconButton>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h5">{title}</Typography>
+        <IconButton
+          onClick={onClose}
+          style={{ marginRight: -12, marginTop: -10 }}
+          disabled={disabled}
+        >
+          <CloseIcon />
+        </IconButton>
+      </Box>
     </DialogTitle>
   );
 }
