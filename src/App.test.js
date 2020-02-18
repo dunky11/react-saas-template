f240d8a3e2c07c04beeb792a462951710792177b
dunky11 <tim.v.kaenel@web.de>
2020-02-18 20:40:02 +0100

    <Pace/> threw errors on test, fixed the error

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("trying to render app", () => {
+test("Trying to render <App />", () => {
   render(<App />);
 });
