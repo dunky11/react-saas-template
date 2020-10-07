51c87541076c9b4cc8c98b1c028d3f7471bc51bd
Marc de Verdelhan <marc.deverdelhan@yahoo.com>
2020-10-07 18:06:38 +0200

    Remove unnecessary parenthesis

--- src/logged_out/components/Routing.js
+++ src/logged_out/components/Routing.js
@@ -30,8 +30,7 @@ function Routing(props) {
         selectBlog={selectBlog}
         blogPosts={blogPosts}
       />
-      )
-      <PropsRoute path="/" component={Home} selectHome={selectHome} />)
+      <PropsRoute path="/" component={Home} selectHome={selectHome} />
     </Switch>
   );
 }
