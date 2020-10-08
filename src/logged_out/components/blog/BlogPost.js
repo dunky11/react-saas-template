902648f32c7f4aa89ab69fb677b03abda266a01b
Marc de Verdelhan <marc.deverdelhan@yahoo.com>
2020-10-08 17:11:12 +0200

    Fix typo

--- src/logged_out/components/blog/BlogPost.js
+++ src/logged_out/components/blog/BlogPost.js
@@ -88,7 +88,7 @@ function BlogPost(props) {
           </Grid>
           <Grid item md={3}>
             <Typography variant="h6" paragraph>
-              Other arcticles
+              Other articles
             </Typography>
             {otherArticles.map((blogPost) => (
               <Box key={blogPost.id} mb={3}>
